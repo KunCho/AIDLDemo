ae73459aaecd81e4c20d5fef57a00e812aedd689
zhaokun <zhao.kun@puhuijia.com>
2018-05-18 17:13:24 +0800

    提交服务端

---
+++ appservice/src/main/java/com/ivivi/appservice/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ivivi.appservice.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ivivi.appservice.R;
+
+/**
+ * Created by zhaokun on 2018/5/18.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
